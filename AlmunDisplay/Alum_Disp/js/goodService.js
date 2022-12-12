fda1094d73feeea9dc5d6347719e1a4cdc4c5811
Max Casteel <maximilian.casteel@umconnect.umt.edu>
2022-12-12 15:19:06 -0700

    Added the fubnctionality to all pages.

--- AlmunDisplay/Alum_Disp/js/goodService.js
+++ AlmunDisplay/Alum_Disp/js/goodService.js
@@ -63,25 +63,42 @@ async function randomizeV2() {
         poplink.innerHTML = "More Info";
         p.appendChild(poplink);
         setpopup(p,i,returnarr2);
-        // create an a
-        var a = document.createElement("a");
-        a.className = "btn btn-primary";
-        a.innerHTML = "Website";
-        a.href = returnarr2[i]["Q3"];
-
-        // append the elements
-
-        teamtext.appendChild(h3);
-        teamtext.appendChild(teamposition);
-        teamtext.appendChild(p);
-        teamtext.appendChild(a);
-
-        teamitem.appendChild(teamimage);
-        teamitem.appendChild(teamtext);
-
-        div.appendChild(teamitem);
-
-        container.appendChild(div);
+        // create an a for the website
+        //if the website3 is empty then dont create the button
+        //if the website link doenst work then dont create the button.  This is a problem with the json file and how people are filling out the survey.
+        //We need to make sure that the website is a valid website, do this by checking if the website has a .com or .org or .net.  If it isn't then dont create the button.
+        if (returnarr2[i]["Q3"] == "" || !returnarr2[i]["Q3"].includes(".") || !returnarr2[i]["Q3"].includes("http")) {
+            teamtext.appendChild(h3);
+            teamtext.appendChild(teamposition);
+            teamtext.appendChild(p);
+            //teamtext.appendChild(a);
+    
+            teamitem.appendChild(teamimage);
+            teamitem.appendChild(teamtext);
+    
+            div.appendChild(teamitem);
+    
+            container.appendChild(div);
+            } else {
+            var a = document.createElement("a");
+            a.className = "btn btn-primary";
+            a.innerHTML = "Website";
+            a.href = returnarr2[i]["Q3"];
+    
+            // append the elements
+    
+            teamtext.appendChild(h3);
+            teamtext.appendChild(teamposition);
+            teamtext.appendChild(p);
+            teamtext.appendChild(a);
+    
+            teamitem.appendChild(teamimage);
+            teamitem.appendChild(teamtext);
+    
+            div.appendChild(teamitem);
+    
+            container.appendChild(div);
+            }
     }
 }
 randomizeV2();
